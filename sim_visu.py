f89d51ea6da0490ce2d075b9fa7420f4705b37cb
catinelb <bennhyon@gmail.com>
2019-05-27 17:28:36 +0200

    ALO

--- sim_visu.py
+++ sim_visu.py
@@ -7,7 +7,7 @@ window_min_height = "500"
 visu_window = Tk()
 
 visu_window.title("Visualisation")
-
+#test
 img = PhotoImage(Image.open("canard.jpg"))
 panel = Label(visu_window, image=img)
 panel.pack(side="bottom", fill="both", expand="yes")
