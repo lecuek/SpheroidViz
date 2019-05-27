11739c95806ba526d34da1ee4db05ba72059a86a
catinelb <bennhyon@gmail.com>
2019-05-27 17:27:00 +0200

    Ajout d'une nouvelle fenêtre (encore WIP)

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ import tkinter as tk
 window_min_width = "500"
 window_min_height = "281"
 
-
-
 main_window = tk.Tk()
 main_window.minsize(width=window_min_width, height=window_min_height)
 main_window.mainloop()
---
+++ sim_visu.py
@@ -0,0 +1,15 @@
+from tkinter import *
+from PIL import Image
+import os
+window_min_width = "500"
+window_min_height = "500"
+
+visu_window = Tk()
+
+visu_window.title("Visualisation")
+
+img = PhotoImage(Image.open("canard.jpg"))
+panel = Label(visu_window, image=img)
+panel.pack(side="bottom", fill="both", expand="yes")
+visu_window.mainloop()
+
