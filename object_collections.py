a8fade2e743fe0033ffa3d7ebc438b6c9ade4ec8
catinelb <bennhyon@gmail.com>
2019-06-03 14:55:53 +0200

    GIT ME CASSE LES

--- object_collections.py
+++ object_collections.py
@@ -1,14 +1,6 @@
 class ObjectCollection:
-<<<<<<< HEAD
     window_collection = {}
     slider_collection = {}
     canvas_collection = {}
     threadings = {}
     button_collection = {}
-=======
-    window_collection = []
-    slider_collection = []
-    canvas_collection = []
-
-        
->>>>>>> dev
