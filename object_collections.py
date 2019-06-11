273acc65b6d470d961db800a47a8efa28159aac7
catinelb <bennhyon@gmail.com>
2019-06-11 17:29:04 +0200

    Can now play the visulalization from x to y

--- object_collections.py
+++ object_collections.py
@@ -1,8 +1,18 @@
+class DictPlus(dict):
+    def __init__(self, _typename, *args, **kwargs):
+        dict.__init__(self, *args, *kwargs)
+        self.type = str(_typename)
+
+    def add(self, _object):
+        self[self.type+"_"+str(len(self))] = _object
+
 class ObjectCollection:
-    windows = {}
-    sliders = {}
-    canvases = {}
-    threadings = {}
-    button_collection = {}
-    visualization_modes = {}
-    
+    windows = DictPlus("Window")
+    sliders = DictPlus("Slider")
+    canvases = DictPlus("Canvas")
+    threadings = DictPlus("Thread")
+    button_collection = DictPlus("Button")
+    visualization_modes = DictPlus("Mode")
+    checkboxes = DictPlus("Checkbox")
+    textinputs = DictPlus("TextInput")
+
