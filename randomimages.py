7402327f215c19917b541811e66d9e4b9f541e32
catinelb <bennhyon@gmail.com>
2019-06-12 16:59:22 +0200

    commit message

--- randomimages.py
+++ randomimages.py
@@ -4,6 +4,7 @@ from numpy import random
 import os
 import sys
 import time
+import threading
 def CreateRandomImages(width=100 ,height=100, n=10, name="image-", extension="png", path="RandomImages/", wait=0, clear=False):
     path = os.path.dirname(os.path.realpath(__file__))+"/"+path
     if clear:
@@ -14,7 +15,7 @@ def CreateRandomImages(width=100 ,height=100, n=10, name="image-", extension="pn
     print("CTRL+C to stop")
     try:
         for i in range(n):
-            dim = random.rand(100,100,3)*255
+            dim = random.rand(100,100,3)*i
             im = Image.fromarray(dim.astype('uint8')).convert("RGBA")
             im.save(path+name+str(i)+"."+extension)
             if wait > 0:
@@ -25,4 +26,6 @@ def CreateRandomImages(width=100 ,height=100, n=10, name="image-", extension="pn
         exit()
     except Exception as e:
         print(e)
-CreateRandomImages(width=500,height=500,n=500,wait=1, extension="jpg",path="Randomedlol/", clear=True)
+#thread = threading.Thread(target=CreateRandomImages, daemon=True, kwargs={"width":500,"height":500,"n":500,"wait":0.2, "extension":"png","path":"Randomed2/", "clear":True})
+#thread.start()
+CreateRandomImages(width=500,height=500,n=500,wait=0.2, extension="png",path="Randomedlol/", clear=True)
